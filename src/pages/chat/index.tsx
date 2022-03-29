dd5acc08d83e52deda60bf93b1d18826c806da27
Artur Haddad <arturhaddad@gmail.com>
2022-03-29 11:48:17 -0300

    feat: add like counter and update it locally

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -23,7 +23,7 @@ const Chat = () => {
   const inputRef = useRef<null | HTMLTextAreaElement>(null);
   const mainChannelRef = useRef<null | Channel>(null);
   const [messages, setMessages] = useState<ChatMessage[]>([]);
-  const [loadingChannelMessages, setLoadingChannelMessages] = useState(false);
+  const [loadingChannelMessages, setLoadingChannelMessages] = useState(true);
   const [loadingSendMessage, setLoadingSendMessage] = useState(false);
   const [newMessage, setNewMessage] = useState('');
   const [userName, setUserName] = useState('');
@@ -36,11 +36,19 @@ const Chat = () => {
 
   const handleLoadMessages = useCallback(async () => {
     if (mainChannelRef.current) {
-      setLoadingChannelMessages(true);
-      const recentMessages = await mainChannelRef.current.loadRecentMessages(
-        20
-      );
-      setMessages(recentMessages);
+      try {
+        setLoadingChannelMessages(true);
+        const recentMessages = await mainChannelRef.current.loadRecentMessages(
+          20
+        );
+        setMessages(recentMessages);
+      } catch (err) {
+        console.error('Error (load messages):', err); // eslint-disable-line no-console
+        alert('Error to load messages. See on console'); // eslint-disable-line no-alert
+      } finally {
+        setLoadingChannelMessages(false);
+        scrollMessagesToBottom();
+      }
     }
   }, []);
 
@@ -53,7 +61,16 @@ const Chat = () => {
 
       mainChannelRef.current.onMessageModified((modifiedMsg) => {
         setMessages((oldValues) =>
-          oldValues.map((m) => (m.key === modifiedMsg.key ? modifiedMsg : m))
+          oldValues.map((m) => {
+            // FIXME: Consider only user reactions changed in local state
+            // due to inconsistent return of currentUserReactions attribute
+            // on onMessageModified event
+            const newMsg = {
+              ...modifiedMsg,
+              currentUserReactions: m.currentUserReactions,
+            };
+            return m.key === modifiedMsg.key ? newMsg : m;
+          })
         );
       });
 
@@ -124,7 +141,7 @@ const Chat = () => {
 
   const handleLikeMsg = async (message: ChatMessage) => {
     if (mainChannelRef.current) {
-      const reactionType = 'love';
+      const reactionType = 'like';
 
       const reaction: MessageReaction = {
         type: reactionType,
@@ -132,17 +149,64 @@ const Chat = () => {
       };
 
       mainChannelRef.current.sendReaction(reaction);
+      // FIXME: Update currentUserReaction and like counter locally due
+      // to inconsistent return of like attributes on onMessageModified event
+      const likeCount = message.reactions?.like || 0;
+      const newLikeCount = likeCount + 1;
+      const likedMessage = {
+        ...message,
+        currentUserReactions: { like: true },
+        reactions: { like: newLikeCount },
+      };
+      const newMessages = messages.map((e) =>
+        e.key === message.key ? likedMessage : e
+      );
+      setMessages(newMessages);
     }
   };
 
-  const userLikedMessage = (message: ChatMessage) => {
-    const isLiked = message.currentUserReactions?.love;
+  const handleDislikeMsg = async (message: ChatMessage) => {
+    if (mainChannelRef.current) {
+      const reactionType = 'like';
+
+      const reaction: MessageReaction = {
+        type: reactionType,
+        messageID: message.key,
+      };
+
+      mainChannelRef.current.deleteReaction(reaction);
+      // FIXME: Update currentUserReaction and like counter locally due
+      // to inconsistent return of like attributes on onMessageModified event
+      const likeCount = message.reactions?.like || 0;
+      const newLikeCount = likeCount === 0 ? 0 : likeCount - 1;
+      const dislikedMsg = {
+        ...message,
+        currentUserReactions: { like: false },
+        reactions: { like: newLikeCount },
+      };
+      const newMessages = messages.map((e) =>
+        e.key === message.key ? dislikedMsg : e
+      );
+      setMessages(newMessages);
+    }
+  };
+
+  const userLikedMsg = (message: ChatMessage) => {
+    const isLiked = message.currentUserReactions?.like;
 
     if (isLiked) {
       return true;
     }
 
     return false;
+  };
+
+  const handleToggleLikeMsg = (message: ChatMessage) => {
+    if (userLikedMsg(message)) {
+      handleDislikeMsg(message);
+    } else {
+      handleLikeMsg(message);
+    }
   };
 
   const stringToColor = (string: string) => {
@@ -161,7 +225,7 @@ const Chat = () => {
   }
 
   useEffect(() => {
-    const chosenUsername = 'ARTUR';
+    const chosenUsername = prompt('Please, enter a username:'); // eslint-disable-line no-alert
     setUserName(chosenUsername || 'Unamed-user-2139');
     inputRef.current?.focus();
   }, []);
@@ -224,13 +288,16 @@ const Chat = () => {
                         </div>
                         <div className="msg-reaction">
                           <button
-                            className={`reaction-button is-liked-${userLikedMessage(
+                            className={`reaction-button is-liked-${userLikedMsg(
                               m
                             )}`}
                             type="button"
-                            onClick={() => handleLikeMsg(m)}
+                            onClick={() => handleToggleLikeMsg(m)}
                           >
-                            ♥
+                            <span className="msg-reaction-icon">♥</span>
+                            <span className="msg-reaction-counter">
+                              {m.reactions?.like || '0'}
+                            </span>
                           </button>
                         </div>
                       </div>
